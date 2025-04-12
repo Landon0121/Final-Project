78092be1fd2f6309d2e632d9ce9515bd12a259ea
LandonG121 <landongarcia0011@gmail.com>
2025-04-12 21:54:54 +0000

    finished

--- jungle.js
+++ jungle.js
@@ -11,11 +11,10 @@ let liquid = prompt("Enter a liquid:")
 let adjective3 = prompt("Enter your third adjective:")
 let food = prompt("Enter a food in plural:");
 let object1 = prompt("Enter an object:");
-let liquid = prompt("Enter a liquid:")
-let adjective3 = prompt("Enter your third adjective:")
+
 
 document.getElementById("output").innerHTML = `
-    <p>It all started when I woke up in the middle of a(n) ${adjective} jungle, clutching nothing but a ${object} and wearing my lucky ${clothing}. The air smelled like ${scent}, and I could hear ${animal} calling in the distance.I took a deep breath and set off, hacking my way through the ${adjective} vines with my trusty [tool]. Suddenly, I tripped over a [noun] and landed face-first in a puddle of [liquid]. Classic me.
+    <p>It all started when I woke up in the middle of a(n) ${adjective} jungle, clutching nothing but a ${object} and wearing my lucky ${clothing}. The air smelled like ${scent}, and I could hear ${animal} calling in the distance.I took a deep breath and set off, hacking my way through the ${adjective} vines with my trusty [tool]. Suddenly, I tripped over a [noun] and landed face-first in a puddle of ${liquid}. Classic me.
     Just as I was getting up, a [adjective] monkey dropped from the trees, wearing sunglasses and eating [plural food item].
     "Hey!" it said. "If you're looking for the legendary [mythical object], you're going the wrong way."
     I didn't even question it—I just followed. We swung from [plural noun], danced with a group of [plural occupation], and narrowly escaped a herd of angry [plural animal].
